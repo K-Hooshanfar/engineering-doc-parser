7b1b33a8396af243971cdba46bea52abb4891676
Kiana Hooshanfar <k.hooshanfar02@gmail.com>
2025-09-27 15:44:14 +0330

    github action rsolved

--- rotation/rotation.py
+++ rotation/rotation.py
@@ -29,7 +29,7 @@ from typing import Iterable, Iterator, List, Sequence, Tuple, Optional
 
 import cv2
 import numpy as np
-import pytesseract  # noqa: F401  # exposed for pkg.pytesseract when run as a script
+import pytesseract  # type: ignore[import-untyped]  # noqa: F401 - used via _pkg() dynamic lookup for standalone execution
 
 
 # ---------- Internal helpers ----------
